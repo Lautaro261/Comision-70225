64e28b5a0e8e26dfc03f7a13fdecda25cbe94554
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2025-01-16 18:43:28 -0300

    visto en clase

---
+++ Clase-03-nuestro-paquete/README.md
@@ -0,0 +1,7 @@
+Paquete de prueba 
+
+
+Comision 70255
+
+console.log(saludo1("nombre"))
+console.log(saludo2("nombre"))
---
+++ Clase-03-nuestro-paquete/src/app.js
@@ -0,0 +1,14 @@
+import colors from "colors";
+
+console.log(`Prueba de text`.bgRed)
+
+
+
+export const saludo1 = nombre => {
+    return nombre.rainbow
+}
+
+
+export const saludo2 = nombre => {
+    return nombre.zebra
+}
